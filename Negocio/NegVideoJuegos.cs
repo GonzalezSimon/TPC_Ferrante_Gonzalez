6466fb1aed2910a1a3d1586d68dee5081cb4b690
Aldebaran14D <gonzalez.simon@yahoo.com.ar>
2021-07-19 18:52:00 -0300

    Front: Carga juegos

---
+++ Negocio/NegVideoJuegos.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Dominio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NegVideoJuegos
+    {
+        public List<VideoJuegos> listar(bool combobox = false)
+        {
+            List<VideoJuegos> lista = new List<VideoJuegos>();
+
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select IDJuego, Nombre, Empresa, Imagen, Estado from Juegos");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    VideoJuegos aux = new VideoJuegos();
+
+                    aux.Id = (int)datos.Lector["IDJuego"];
+                    //carga de TipoServicio
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Empresa = (string)datos.Lector["Empresa"];
+                    aux.Imagen = (string)datos.Lector["Imagen"];
+
+                    aux.Estado = (bool)datos.Lector["Estado"];
+                    
+
+                    if (aux.Estado == true) lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+        public void agregar(VideoJuegos nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+                datos.setearConsulta("insert into Juegos (Nombre, Empresa, Imagen, Estado)" +
+                " VALUES (@Nombre, @Empresa, @Imagen, @Estado)");
+
+                //datos.agregarParametro("@IDJuego", nuevo.Id);
+                datos.agregarParametro("@Nombre", nuevo.Nombre);
+                datos.agregarParametro("@Empresa", nuevo.Empresa);
+                datos.agregarParametro("@Imagen", nuevo.Imagen);
+                datos.agregarParametro("@Estado", nuevo.Estado);
+
+                datos.ejectutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(VideoJuegos modificar)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta(
+                "update Juegos set IDJuego = @IDJuego, Nombre = @Nombre, Empresa = @Empresa, Imagen = @Imagen, Estado = @Estado WHERE IDJuego =" + modificar.Id);
+
+                datos.agregarParametro("@IDJuego", modificar.Id);
+                datos.agregarParametro("@Nombre", modificar.Nombre);
+                datos.agregarParametro("@Empresa",modificar.Empresa);
+                datos.agregarParametro("@Imagen", modificar.Imagen);
+                datos.agregarParametro("@Estado", modificar.Estado);
+
+                datos.ejectutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int ID)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                VideoJuegos aux = new VideoJuegos();
+
+                aux.Id = ID;
+                aux.Estado = false;
+
+                datos.setearConsulta(
+                    "update Juegos set Estado = 0 where IDJuego = " + aux.Id);
+
+                datos.ejectutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
+
---
+++ TPC_Ferrante_Gonzalez/CargaJuegos.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_Ferrante_Gonzalez
+{
+
+
+    public partial class CargaJuegos
+    {
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtEmpresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmpresa;
+
+        /// <summary>
+        /// Control FileUpload1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+
+        /// <summary>
+        /// Control btnIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngresar;
+    }
+}
